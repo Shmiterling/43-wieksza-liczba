d121511b217a64d9dd2da83df76abfe449cbb5bf
dbessarab <d.bessarab@gmail.com>
2022-02-22 20:46:20 +0100

    43.1

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function myFunction(num1, num2,) {
   if (num1 > num2) {
     return num1;
   } else if (num1 == num2) {
-    console.log("Liczby są równe!")
+    return "Liczby są równe!";
   } else {
     return num2;
   }
